856fc7a63c2a0bed056d187785f1e93a9580f103
answhdtj0620 <answhdtj0620@gmail.com>
2021-04-06 18:36:24 +0900

    Connect macchu to recommend_by_person

--- Django/macchu/macchu/urls.py
+++ Django/macchu/macchu/urls.py
@@ -21,5 +21,5 @@ from .views import HomeView
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', HomeView.as_view(), name="home"),
+    path('', include('recommend_by_person.urls')),
 ]
---
+++ Django/macchu/recommend_by_person/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RecommendByPersonConfig(AppConfig):
+    name = 'recommend_by_person'
