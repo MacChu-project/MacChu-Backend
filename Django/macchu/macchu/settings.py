d2423eb939ac58b8c82023e2265e5bbfd0cc45cd
HYEONGSEON SON <gudtjs0428@gmail.com>
2021-04-12 14:58:49 +0900

    settings.py-databses

--- Django/macchu/macchu/settings.py
+++ Django/macchu/macchu/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'macchu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'macchu-db.cmq0yxrruhsw.ap-northeast-2.rds.amazonaws.com',
+        'PORT': 3306,
+        'NAME': 'MacChu',
+        'USER': 'admin',
+        'PASSWORD': 'a123456789'
     }
 }
 
