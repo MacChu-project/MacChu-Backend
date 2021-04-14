4501afe6433267cb19540c7140e4b96ff22d9c70
ehhahappy <mqmqmqmqmq@naver.com>
2021-04-14 16:03:21 +0900

    Add table

--- DB/create_table.sql
+++ DB/create_table.sql
@@ -24,3 +24,14 @@ PRIMARY KEY (`beer_idx`),
 KEY `fkIdx_186` (`beer_idx`),
 CONSTRAINT `FK_185` FOREIGN KEY `fkIdx_186` (`beer_idx`) REFERENCES `beer` (`beer_idx`)
 );
+
+CREATE TABLE `beer_rank`
+(
+ `beer_idx`         int NOT NULL ,
+ `user_click_count` int NOT NULL ,rankbeer_rank
+ `recommend_count`  int NOT NULL ,
+
+PRIMARY KEY (`beer_idx`),
+KEY `fkIdx_196` (`beer_idx`),
+CONSTRAINT `FK_195` FOREIGN KEY `fkIdx_196` (`beer_idx`) REFERENCES `beer` (`beer_idx`)
+);
