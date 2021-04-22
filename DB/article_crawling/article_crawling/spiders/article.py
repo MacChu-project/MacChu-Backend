91b5a3aba5d5d71638354edfbba57fb7300d629c
ehhahappy <mqmqmqmqmq@naver.com>
2021-04-23 00:36:30 +0900

    Fix url and tag

--- DB/article_crawling/article_crawling/spiders/article.py
+++ DB/article_crawling/article_crawling/spiders/article.py
@@ -7,9 +7,8 @@ from article_crawling.items import ArticleCrawlingItem
 class ArticleSpider(scrapy.Spider):
     name = 'article'
     # allowed_domains = ['naver.com']
-    url_format = "https://search.naver.com/search.naver?&where=news&query={0}&sm=tab_pge&sort=0&photo=0&field=0&reporter_article=&pd=3&ds={1}&de={1}&docid=&nso=so:r,p:from{2}to{2},a:all&mynews=0&cluster_rank=41&start=1&refresh_start=0"
-                # https://search.naver.com/search.naver?where=news&query={0}&sm=tab_opt&sort=0&photo=0&field=0&pd=3&ds=2021.04.21&de=2021.04.21&docid=&related=0&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so%3Ar%2Cp%3Afrom20210421to20210421
- 
+    url_format = "https://search.naver.com/search.naver?where=news&sm=tab_pge&query={0}&sort=0&photo=0&field=0&pd=3&ds={1}&de={1}&cluster_rank=28&office_section_code=0&news_office_checked=&nso=so:r,p:from{2}to{2},a:all&start=1"
+                 
     def __init__(
         self, keyword="", start="", end="", **kwargs
     ):
@@ -21,16 +20,17 @@ class ArticleSpider(scrapy.Spider):
             self.start_urls.append(self.url_format.format(keyword, cur_date.strftime("%Y.%m.%d"), cur_date.strftime("%Y%m%d")))
  
     def parse(self, response):
-        for item in response.css("ul.type01 li") :
-            if item.css("a._sp_each_url") :
-                url = item.css("a._sp_each_url::attr(href)").get()
+        for item in response.css("ul.list_news li") :
+            if item.css("a.info") :
+                url_list = item.css("a.info::attr(href)").getall()
+                url = url_list[1]
                 yield scrapy.Request(url, callback=self.parse_detail)
         
-        next_page = response.css('a.next::attr(href)').get()
+        next_page = response.css('a.btn_next::attr(href)').get()
         if next_page is not None:
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
-    
+
     def parse_detail(self, response):   
         item = ArticleCrawlingItem()
         item['url']=response.url
@@ -38,6 +38,5 @@ class ArticleSpider(scrapy.Spider):
         item['text']=''.join(response.css("div#articleBodyContents::text").getall()).replace("\n","").strip()
         item['date']=response.css("div.sponsor span.t11::text").get()
         item['logo']=response.css("div.press_logo img::attr(title)").get()
-        
         
         yield item
