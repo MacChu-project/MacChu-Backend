4a4db1c2bbb8e913cfe7b9eb3fdf90274decf2be
ehhahappy <mqmqmqmqmq@naver.com>
2021-04-26 19:13:51 +0900

    Add news table

--- DB/create_table.sql
+++ DB/create_table.sql
@@ -28,10 +28,23 @@ CONSTRAINT `FK_185` FOREIGN KEY `fkIdx_186` (`beer_idx`) REFERENCES `beer` (`bee
 CREATE TABLE `beer_rank`
 (
  `beer_idx`         int NOT NULL ,
- `user_click_count` int NOT NULL ,rankbeer_rank
+ `user_click_count` int NOT NULL ,
  `recommend_count`  int NOT NULL ,
 
 PRIMARY KEY (`beer_idx`),
 KEY `fkIdx_196` (`beer_idx`),
 CONSTRAINT `FK_195` FOREIGN KEY `fkIdx_196` (`beer_idx`) REFERENCES `beer` (`beer_idx`)
 );
+
+CREATE TABLE `beer_news`
+(
+ `beer_news_idx`   int NOT NULL ,
+ `news_date`       datetime NOT NULL ,
+ `news_title`      varchar(45) NOT NULL ,
+ `news_content`    text NOT NULL ,
+ `news_url`        varchar(200) NOT NULL ,
+ `news_view_count` int NOT NULL DEFAULT 0,
+
+PRIMARY KEY (`beer_news_idx`)
+);
+
